0aea43d2816e6422e295e7ec0d191889edd05b5f
prevail312 <Prevail312@outlook.com>
2024-03-05 17:35:19 -0600

    initial commit

---
+++ CurrentTimeService/WeatherForecast.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CurrentTimeService
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+        public DateTime Time { get; internal set; }
+    }
+}
